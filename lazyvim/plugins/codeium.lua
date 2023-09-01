bf366ae7de4c25555b40fdfb36436f75ba075a06
Nick Smith <ndaman@gmail.com>
2023-09-01 13:00:55 -0700

    switch to main codeium
    Using the main codeium.vim plugin, changes keymaps (c+g to accept codeium), works together with lsp

--- lazyvim/plugins/codeium.lua
+++ lazyvim/plugins/codeium.lua
@@ -1,42 +1,27 @@
 -- look at open source alternative: https://github.com/dense-analysis/neural
 -- main plugin
--- return {
---   "Exafunction/codeium.vim",
---   config = function()
---     -- change keymaps if desired
---     vim.keymap.set("i", "<C-g>", function()
---       return vim.fn["codeium#Accept"]()
---     end, { expr = true })
---     vim.keymap.set("i", "<c-;>", function()
---       return vim.fn["codeium#CycleCompletions"](1)
---     end, { expr = true })
---     vim.keymap.set("i", "<c-,>", function()
---       return vim.fn["codeium#CycleCompletions"](-1)
---     end, { expr = true })
---     vim.keymap.set("i", "<c-x>", function()
---       return vim.fn["codeium#Clear"]()
---     end, { expr = true })
---   end,
--- }
--- neovim native plugin
 return {
-  {
-    "jcdickinson/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({})
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "jcdickinson/codeium.nvim" },
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "codeium" } }))
-    end,
-  },
+  "Exafunction/codeium.vim",
+  config = function()
+    -- change keymaps if desired
+    vim.keymap.set("i", "<C-g>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true })
+    vim.keymap.set("i", "<c-;>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true })
+    vim.keymap.set("i", "<c-,>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true })
+    vim.keymap.set("i", "<c-x>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true })
+  end,
 }
+-- neovim native plugin
+-- return {
+--   {
+--     "Exafunction/codeium.vim",
+--     event = "BufEnter",
+--   },
+-- }
