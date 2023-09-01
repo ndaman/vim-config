92057f4d8c93694cbdad07e255fc8a65f7bd6e6f
Nick Smith <ndaman@gmail.com>
2023-09-01 13:01:49 -0700

    new defaults
    I think this is just a change to the template provided by lazyvim

---
+++ lazyvim/plugins/telescope.lua
@@ -0,0 +1,10 @@
+-- change some telescope options and a keymap to browse plugin files
+return {
+  "nvim-telescope/telescope.nvim",
+  -- apply the config and additionally load fzf-native
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("notify")
+  end,
+}
