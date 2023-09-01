b8a0e7f345e673a569b6fef6f160deb10ebe0b65
Nick Smith <ndaman@gmail.com>
2023-09-01 13:00:08 -0700

    change wsl clipboard
    changed neovim config to work better on Windows (this is much better when using Neovide on Windows)

--- lazyvim/config/options.lua
+++ lazyvim/config/options.lua
@@ -7,12 +7,25 @@ vim.g.maplocalleader = ","
 -- set clipboard for wsl
 -- to use win32yank needs to be installed in path
 -- see https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
-if vim.fn.has("wsl") == 1 then
-  vim.opt.clipboard = "unnamedplus"
-  vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
-    callback = function()
-      vim.fn.system("win32yank.exe", vim.fn.getreg('"'))
-    end,
-  })
-end
+--if vim.fn.has("wsl") == 1 then
+--  vim.opt.clipboard = "unnamedplus"
+--  vim.api.nvim_create_autocmd("TextYankPost", {
+--    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+--    callback = function()
+--      vim.fn.system("win32yank.exe", vim.fn.getreg('"'))
+--    end,
+--  })
+--end
+-- alternative clipboard settings
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
