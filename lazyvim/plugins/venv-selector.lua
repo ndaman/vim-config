200997d99e6ed3d0cdb49dd5c6392414c4f71245
Nick Smith <ndaman@gmail.com>
2023-09-01 13:02:20 -0700

    venv selector
    added venv-selector plugin, this makes working with different python env's inside neovim much easier

---
+++ lazyvim/plugins/venv-selector.lua
@@ -0,0 +1,22 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  opts = {
+    -- Your options go here
+    -- name = "venv",
+    -- auto_refresh = false
+    anaconda_base_path = "C:\\Users\\ndama\\mambaforge",
+    anaconda_envs_path = "C:\\Users\\ndama\\mambaforge\\envs",
+  },
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    {
+      -- Keymap to open VenvSelector to pick a venv.
+      "<leader>vs",
+      "<cmd>:VenvSelect<cr>",
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      "<leader>vc",
+      "<cmd>:VenvSelectCached<cr>",
+    },
+  },
+}
