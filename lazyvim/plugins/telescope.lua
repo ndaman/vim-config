6948d53c9bd3627355817795a5f8f8e26cd1b13e
Nick <ndaman@gmail.com>
2024-12-15 11:45:54 -0800

    updated new lazyvim stuff

--- lazyvim/plugins/telescope.lua
+++ lazyvim/plugins/telescope.lua
@@ -5,6 +5,6 @@ return {
   config = function(_, opts)
     local telescope = require("telescope")
     telescope.setup(opts)
-    telescope.load_extension("notify")
+    -- telescope.load_extension("notify")
   end,
 }
